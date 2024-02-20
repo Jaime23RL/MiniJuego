756cc20b3a4ff6f79db69381f99f9d02445e742a
Jaime23RL <jaime.rubio.lopez@ces-vegamedia.es>
2024-02-20 11:54:08 +0100

    Todo hecho primera diapositiva

--- VegaJuego/Level1.cs
+++ VegaJuego/Level1.cs
@@ -1,10 +1,25 @@
+using System.Diagnostics;
+
 namespace VegaJuego
 {
     public partial class Level1 : Form
     {
+        Link Player;
         public Level1()
         {
             InitializeComponent();
+            IniciarPersonajes();
+        }
+
+        private void IniciarPersonajes()
+        {
+            Player = new Link(this, 0, 0, 30, 0, 0);
+        }
+
+        private void Level1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Player.mover(e);
+            Debug.WriteLine("[x" + Player.Caja.Location.X + ",Y" + Player.Caja.Location.Y + "]");
         }
     }
 }
